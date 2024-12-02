966f06e6ab1be5176a8feacc70635505d5c54486
Jay Palkar <110560844+JayPalkar@users.noreply.github.com>
2024-12-02 09:12:58 +0530

    custom cursor completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./app.css";
+import { useEffect } from "react";
 import AboutMe from "./components/aboutMe/page";
 import ContactMe from "./components/contactme/page";
 import Hero from "./components/hero/page";
@@ -7,15 +8,43 @@ import Projects from "./components/projects/page";
 import Testimonial from "./components/Testimonial/page";
 
 const App = () => {
+  useEffect(() => {
+    const cursorBackdrop = document.querySelector(".cursorBackdrop");
+
+    if (!cursorBackdrop) return;
+
+    document.addEventListener("mouseenter", () => {
+      cursorBackdrop.style.display = "block";
+    });
+
+    document.addEventListener("mouseleave", () => {
+      cursorBackdrop.style.display = "none";
+    });
+
+    const moveCursor = (e) => {
+      cursorBackdrop.style.left = `${e.pageX}px`;
+      cursorBackdrop.style.top = `${e.pageY}px`;
+    };
+
+    window.addEventListener("mousemove", moveCursor);
+
+    return () => {
+      window.removeEventListener("mousemove", moveCursor);
+    };
+  }, []);
+
   return (
-    <div>
-      <TopNavbar />
-      <Hero />
-      <AboutMe />
-      <Projects />
-      <Testimonial />
-      <ContactMe />
-    </div>
+    <>
+      <div className="cursorBackdrop"></div>
+      <div className="mainAppContent">
+        <TopNavbar />
+        <Hero />
+        <AboutMe />
+        <Projects />
+        <Testimonial />
+        <ContactMe />
+      </div>
+    </>
   );
 };
 
