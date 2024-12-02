5ad0edb0d7b94308e7926e21a4d19ed28a9a0433
Jay Palkar <110560844+JayPalkar@users.noreply.github.com>
2024-12-02 08:28:56 +0530

    Testimonial header setup

--- src/components/Testimonial/page.jsx
+++ src/components/Testimonial/page.jsx
@@ -1,5 +1,14 @@
+import "./Testimonial.css";
+
 const Testimonial = () => {
-  return <div>Testimonial</div>;
+  return (
+    <div className="testimonialSection">
+      <div className="testimonialHeader">
+        <h1 className="backDrop">Testimonial</h1>
+        <h1 className="fair">Testimonial</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Testimonial;
