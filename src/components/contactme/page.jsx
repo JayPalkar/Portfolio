4e3505a193c1997dfc2e0df660ddf9506a0614f4
Jay Palkar <110560844+JayPalkar@users.noreply.github.com>
2024-12-08 14:05:34 +0530

    contact section responsiveness completed

--- src/components/contactme/page.jsx
+++ src/components/contactme/page.jsx
@@ -2,6 +2,7 @@ import { IoMdMail } from "react-icons/io";
 import "./contact.css";
 import { MdPhone } from "react-icons/md";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { RxDoubleArrowRight } from "react-icons/rx";
 
 const ContactMe = () => {
   return (
@@ -40,22 +41,7 @@ const ContactMe = () => {
           ></textarea>
           <div className="buttonContainer">
             <button>
-              <svg
-                width="64"
-                height="64"
-                viewBox="0 0 64 64"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M41.3333 13.3333H29.3333L42.6667 31.9999L29.3333 50.6666H41.3333L54.6667 31.9999L41.3333 13.3333Z"
-                  fill="#E3CA3C"
-                />
-                <path
-                  d="M22.6667 13.3333H10.6667L24 31.9999L10.6667 50.6666H22.6667L36 31.9999L22.6667 13.3333Z"
-                  fill="#E3CA3C"
-                />
-              </svg>
+              <RxDoubleArrowRight />
             </button>
           </div>
         </div>
