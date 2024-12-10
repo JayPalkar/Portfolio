11694ad8a8a2bc2dcbe3e4a28c08266699f47b4e
Jay Palkar <110560844+JayPalkar@users.noreply.github.com>
2024-12-10 18:25:49 +0530

    added hamburger Icon

--- src/components/navbar/sideNavbar.jsx
+++ src/components/navbar/sideNavbar.jsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
 import "./sideNavbar.css";
 
 const SideNavbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuOpen = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <>
       <div className="sidebarContainer">
         <a className="brandName" href="#hero">
           J.
         </a>
+        <button onClick={handleMenuOpen} className="hamburgerButton">
+          <div
+            className={`hamburgerBar topBar ${
+              menuOpen ? "transformToCross" : ""
+            }`}
+          ></div>
+          <div
+            className={`hamburgerBar bottomBar ${
+              menuOpen ? "transformToCross" : ""
+            }`}
+          ></div>
+        </button>
         <a className="sidebarLinks" href="#about">
           About
         </a>
